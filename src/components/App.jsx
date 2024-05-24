308bb12c22414be2fd1b4977280542033035cbe9
spider-guru <preciousgodwin559@gmail.com>
2024-05-24 14:02:37 +0100

    feature: added ability to save and recall last theme uses in the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import './style.css'
+import "./style.css";
 import Header from "./Header";
 import Form from "./Form";
 import TodoList from "./TodoList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 /*
 features to be added:
@@ -16,20 +16,42 @@ function App() {
 	let [todoList, setTodoList] = useState([]);
 	let [isErr, setIsErr] = useState(false);
 	let [isDark, setIsDark] = useState(false);
+
 	const spring = {
 		type: "spring",
 		stiffness: 200,
 		damping: 10,
 		duration: 3,
 	};
+
+	useEffect(() => {
+		try {
+			if (localStorage.getItem("todoTheme") == null) {
+				localStorage.setItem("todoTheme", "light");
+				setIsDark((ini) => (ini = false));
+			} else if (localStorage.getItem("todoTheme") == "dark") {
+				setIsDark((ini) => (ini = true));
+			} else if (localStorage.getItem("todoTheme") == "light") {
+				setIsDark(false);
+			}
+		} catch (error) {
+			console.info("error from setting theme in localstorage");
+		}
+	}, []);
+
+	useEffect(() => {
+		let data = isDark ? "dark" : "light";
+		localStorage.setItem("todoTheme", data);
+	}, [isDark]);
+
 	return (
-		<div className={isDark ? 'bodyD' : 'bodyL'}>
+		<div className={isDark ? "bodyD" : "bodyL"}>
 			<motion.div
 				initial={{ opacity: 0, scale: 0 }}
 				animate={{ opacity: 1, scale: 1 }}
 				transition={spring}
 				layout
-				className={isDark ? 'AppD' : 'AppL'}
+				className={isDark ? "AppD" : "AppL"}
 			>
 				<Header isDark={isDark} setIsDark={setIsDark} />
 				<Form
@@ -45,9 +67,9 @@ function App() {
 
 				<p>
 					{isErr ? (
-						<span className={'errMsg'}>Pls fill in a valid task, no empty task allowed</span>
+						<span className={"errMsg"}>Pls fill in a valid task, no empty task allowed</span>
 					) : (
-						<span className={'info'}>Built by Spider-Guru🕷</span>
+						<span className={"info"}>Built by Spider-Guru🕷</span>
 					)}
 				</p>
 			</motion.div>
